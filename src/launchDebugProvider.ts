0577210614ed534f28460807dc62cc9bf873f48f
Michael Liao <michael.liao@microsoft.com>
2020-04-27 14:19:27 -0700

    Rename extension

--- src/launchDebugProvider.ts
+++ src/launchDebugProvider.ts
@@ -41,7 +41,7 @@ export default class LaunchDebugProvider implements vscode.DebugConfigurationPro
         folder: vscode.WorkspaceFolder | undefined,
         token?: vscode.CancellationToken): vscode.ProviderResult<vscode.DebugConfiguration[]> {
         return Promise.resolve([{
-            name: "Launch Microsoft Edge and open the Elements tool",
+            name: "Launch Microsoft Edge and open the DevTools",
             request: "launch",
             type: `${SETTINGS_STORE_NAME}.debug`,
             url: "http://localhost:8080",
