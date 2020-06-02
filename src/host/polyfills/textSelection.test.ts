e75094b4952475e647203a802f727e83b73233b1
Michael Liao <michael.liao@microsoft.com>
2020-06-02 11:56:18 -0700

    TextSelection polyfill refactor

--- src/host/polyfills/textSelection.test.ts
+++ src/host/polyfills/textSelection.test.ts
@@ -1,17 +1,25 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { getTextFromFile } from "../../test/helpers";
 
 describe("textSelection", () => {
     it("applySetupTextSelectionPatch correctly changes text", async () => {
+        const comparableText = "_setupTextSelectionHack(stylePaneWrapperElement) { code }";
+        let fileContents = getTextFromFile("elements/ElementsPanel.js");
+
+        // The file was not found, so test that at least the text is being replaced.
+        fileContents = fileContents ? fileContents : comparableText;
+
         const apply = await import("./textSelection");
-        const result = apply.default("_setupTextSelectionHack(stylePaneWrapperElement) { code }");
-        expect(result).toEqual("_setupTextSelectionHack() { return; code }");
+        const result = apply.default(fileContents);
+        expect(result).not.toEqual(null);
+        expect(result).toEqual(expect.stringContaining("_setupTextSelectionHack() { return;"));
     });
 
     it("applySetupTextSelectionPatch ignores other text", async () => {
         const apply = await import("./textSelection");
         const expectedText = "hello world";
         const result = apply.default(expectedText);
-        expect(result).toEqual(expectedText);
+        expect(result).toEqual(null);
     });
 });
