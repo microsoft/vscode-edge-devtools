56ca1e20afadc1949156a760ff3fafe85a11e7ce
Michael Liao <michael.liao@microsoft.com>
2020-06-04 14:06:02 -0700

    inspectorContentPolicy polyfill refactor

--- src/host/polyfills/inspectorContentPolicy.test.ts
+++ src/host/polyfills/inspectorContentPolicy.test.ts
@@ -1,18 +1,21 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+import { getTextFromFile } from "../../test/helpers";
 
 describe("inspectorContentPolicy", () => {
     it("applyContentSecurityPolicyPatch correctly changes text", async () => {
+        //const comparableText = "script-src 'self' 'unsafe-eval' <meta name=\"referrer\" content=\"no-referrer\">";
+        let fileContents = getTextFromFile("inspector.html");
+
+        // The file was not found, so test that at least the text is being replaced.
+        fileContents = fileContents ? fileContents : "aaa";
+
         const apply = await import("./inspectorContentPolicy");
-        const result = apply.applyContentSecurityPolicyPatch(
-            `Content-Security-Policy" content="script-src 'self' 'unsafe-eval'"`);
+        const result = apply.applyContentSecurityPolicyPatch(fileContents);
         expect(result).toEqual(
             expect.stringContaining(
-                `Content-Security-Policy" content="script-src vscode-resource: 'self' 'unsafe-eval'"`));
-
-        const result2 = apply.applyContentSecurityPolicyPatch(
-            `<meta name="referrer" content="no-referrer">`);
-        expect(result2).toEqual(
+                `script-src vscode-resource: 'self'`));
+        expect(result).toEqual(
             expect.stringContaining(
                 `<script src="../../host/host.bundle.js"></script>`));
     });
