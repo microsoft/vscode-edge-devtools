b7a1c434d05d1a85f0cd836b081c3cc9c0f1907f
Michael Liao <michael.liao@microsoft.com>
2020-06-04 17:33:15 -0700

    Update retrieveDownloadLink to differentiate platforms

--- scripts/retrieveDownloadLink.js
+++ scripts/retrieveDownloadLink.js
@@ -6,14 +6,29 @@ function fetchJsonFromUrl(url){
   return Httpreq.responseText;
 }
 
-function fetchDownloadUrl() {
+function fetchDownloadUrl(platform) {
+  if (!platform) {
+    console.log('Invalid platform');
+    return;
+  }
   const jsonString = fetchJsonFromUrl("https://thirdpartysource.microsoft.com/downloads");
   const jsonObjects = JSON.parse(jsonString);
   for (let object of jsonObjects) {
-    if (object.product === 'Microsoft Edge DevTools' && object.release === '81.0.416.72') {
+    if (object.product === 'Microsoft Edge DevTools' && object.release === '81.0.416.72' && object.platform === platform) {
       console.log(object.url);
     }
   }
 }
 
-fetchDownloadUrl();
+function retrievePlatform() {
+  const arg = process.argv.slice(2)[0];
+  switch (arg) {
+    case 'win':
+      return 'Windows x64';
+    case 'mac':
+      return 'Mac OS x64';
+  }
+}
+
+const platform = retrievePlatform();
+fetchDownloadUrl(platform);
