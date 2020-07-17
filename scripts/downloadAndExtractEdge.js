bab92fe1abc1acf69be2442fd561175677cbffde
Michael Liao <michael.liao@microsoft.com>
2020-07-17 13:18:36 -0700

    [Download Script]: Improving mac contributing workflow (#174)
    * [WiP]: Improving mac contributing workflow
    
    * updating downloadAndExtractEdge
    
    * mac fixes
    
    * fix
    
    * mac fix 2
    
    * Updating contributing doc
    
    * Minor fixes in contributing and download script
    
    * Updating contributing doc
    
    Co-authored-by: Vidal Ortega <43078681+vidorteg@users.noreply.github.com>

--- scripts/downloadAndExtractEdge.js
+++ scripts/downloadAndExtractEdge.js
@@ -1,4 +1,5 @@
 const TARGET_VERSION = '83.0.478.45';
+var isWindows = true;
 
 function fetchJsonFromUrl(url){
   var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
@@ -12,6 +13,7 @@ function fetchDownloadUrl(platform) {
   const jsonString = fetchJsonFromUrl('https://thirdpartysource.microsoft.com/downloads');
   const jsonObjects = JSON.parse(jsonString);
   const platformString = retrievePlatform(platform);
+  console.log('Downloading Microsoft Edge DevTools version ' + TARGET_VERSION + ' for ' + platformString);
   for (let object of jsonObjects) {
     if (object.product === 'Microsoft Edge DevTools' && object.release === TARGET_VERSION && object.platform === platformString) {
       return object.url;
@@ -21,21 +23,28 @@ function fetchDownloadUrl(platform) {
 
 function retrievePlatform(platform) {
   if (!platform) {
-    return 'Windows x64';
+    switch (process.platform) {
+      case 'win32':
+        return 'Windows x64';
+      default:
+        isWindows = false;
+        return 'Mac OS x64';
+    }
   }
   switch (platform.toLowerCase()) {
-    case 'mac':
-      return 'Mac OS x64';
-    default:
+    case 'win':
       return 'Windows x64';
+    default:
+      isWindows = false;
+      return 'Mac OS x64';
   }
 }
 
-function removeLastDirectory(filepath)
-{
-    var arr = filepath.split('\\');
+function removeLastDirectory(filepath) {
+    const splitChar = isWindows ? '\\' : '/';
+    var arr = filepath.split(splitChar);
     arr.pop();
-    return( arr.join('\\') );
+    return( arr.join(splitChar) );
 }
 
 async function downloadZipFile(downloadUrl) {
@@ -50,10 +59,18 @@ async function downloadZipFile(downloadUrl) {
     response.on('end', ()=>{
       fs.createReadStream('edge.zip').pipe(unzipper.Extract({path: 'out/edge/'}));
       fs.unlink('edge.zip', () => {} );
-      const flipSlashDirName = __dirname.replace(/\//g, '\\');
-      const rootPath = removeLastDirectory(flipSlashDirName);
-      console.log('Edge files extracted to: ' + rootPath + '\\out\\edge');
-      console.log('Run this in cmd: "set EDGE_CHROMIUM_PATH=' + rootPath + '\\out\\edge\\src&&set EDGE_CHROMIUM_OUT_DIR=Release"');
+      let dirName = __dirname;
+      if (isWindows) {
+        flipSlashDirName = dirName.replace(/\//g, '\\');
+        const rootPath = removeLastDirectory(flipSlashDirName);
+        console.log('Edge files extracted to: ' + rootPath + '\\out\\edge\n');
+        console.log('Run this in cmd to set env variables: "set EDGE_CHROMIUM_PATH=' + rootPath + '\\out\\edge\\src&&setx EDGE_CHROMIUM_OUT_DIR=Release"\n');
+      } else {
+        const rootPath = removeLastDirectory(dirName);
+        console.log('Edge files extracted to: ' + rootPath + '/out/edge');
+        console.log('Run this in terminal to set env variables: "export EDGE_CHROMIUM_PATH=' + rootPath + '/out/edge/src&&export EDGE_CHROMIUM_OUT_DIR=Release"\n');
+      }
+      console.log('*Note: this command only sets the environment variables for this session.');
     });
   });
 }
