b155ec866c91fe2a985098b656185fdd5d0806e6
Michael Liao <michael.liao@microsoft.com>
2021-03-03 12:36:32 -0800

    fix

--- src/host/polyfills/releaseNote.test.ts
+++ src/host/polyfills/releaseNote.test.ts
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+
 import { getTextFromFile } from "../../test/helpers";
 import * as ReleaseNotePatch from "./releaseNote";
 
