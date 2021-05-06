0292089119a0bd0944343c05c3008a74a3235622
Vidal Guillermo Diazleal Ortega <vidorteg@microsoft.com>
2021-05-06 16:17:55 -0700

    fixing tests

--- test/launchConfigManager.test.ts
+++ test/launchConfigManager.test.ts
@@ -1,68 +1,59 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import {
-    createFakeVSCode,
-    Mocked,
-} from "./helpers/helpers";
-import { LaunchConfigManager, providedDebugConfig } from "../src/LaunchConfigManager";
+import {createFakeVSCode} from "./helpers/helpers";
+import { LaunchConfigManager, providedDebugConfig} from "../src/LaunchConfigManager";
 
 jest.mock("vscode", () => createFakeVSCode(), { virtual: true });
 jest.mock("fs-extra");
 
 describe("launchConfigManager", () => {
+
     describe('getLaunchJson', () => {
-        let fse: Mocked<typeof import("fs-extra")>;
-        let launchConfigManager: LaunchConfigManager;
-
-        beforeEach(async () => {
-            fse = require("fs-extra");
-            launchConfigManager = LaunchConfigManager.instance;
-        });
-
         it('updates launchJsonStatus with "None" when launch.json does not exist', async () => {
-            const vscodeMock = await jest.requireMock("vscode");
+            const vscodeMock = jest.requireMock("vscode");
+            const fse = jest.requireMock("fs-extra");
+            vscodeMock.workspace.getConfiguration.mockImplementation(() => {
+                return {
+                    get: (name: string) => [{type: 'vscode-edge-devtools.debug'}]
+                }
+            });
             fse.pathExistsSync.mockImplementation(() => false);
-            expect(launchConfigManager.getLaunchConfig()).toEqual('None');
-            expect(vscodeMock.commands.executeCommand).toBeCalledWith('setContext', 'launchJsonStatus', 'None');
-        });
-
-        it('updates launchJsonStatus with "None" when launch.json does not exist', async () => {
-            const vscodeMock = await jest.requireMock("vscode");
             const launchConfigManager = LaunchConfigManager.instance;
-            fse.pathExistsSync.mockImplementation(() => false);
             expect(launchConfigManager.getLaunchConfig()).toEqual('None');
             expect(vscodeMock.commands.executeCommand).toBeCalledWith('setContext', 'launchJsonStatus', 'None');
         });
 
         it('updates launchJsonStatus with "Unsupported" when there is no supported debug config', async () => {
-            const vscodeMock = await jest.requireMock("vscode");
-            // eslint-disable-next-line no-console
+            const vscodeMock = jest.requireMock("vscode");
+            const fse = jest.requireMock("fs-extra");
             fse.pathExistsSync.mockImplementation(() => true);
             vscodeMock.workspace.getConfiguration.mockImplementation(() => {
                 return {
                     get: (name: string) => [{type: ''}]
                 }
             });
+            const launchConfigManager = LaunchConfigManager.instance;
             expect(launchConfigManager.getLaunchConfig()).toEqual('Unsupported');
             expect(vscodeMock.commands.executeCommand).toBeCalledWith('setContext', 'launchJsonStatus', 'Unsupported');
         });
 
         it('returns a supported debug config when one exists', async () => {
-            const vscodeMock = await jest.requireMock("vscode");
-            const launchConfigManager = LaunchConfigManager.instance;
+            const vscodeMock = jest.requireMock("vscode");
+            const fse = jest.requireMock("fs-extra");
             fse.pathExistsSync.mockImplementation(() => true);
             vscodeMock.workspace.getConfiguration.mockImplementation(() => {
                 return {
                     get: (name: string) => [{type: 'vscode-edge-devtools.debug'}]
                 }
             });
+            const launchConfigManager = LaunchConfigManager.instance;
             expect(launchConfigManager.getLaunchConfig()).toEqual({type: 'vscode-edge-devtools.debug'});
             expect(vscodeMock.commands.executeCommand).toBeCalledWith('setContext', 'launchJsonStatus', 'Supported');
         });
 
         it('updates launchJsonStatus with "None" when there is no folder open', async () => {
-            const vscodeMock = await jest.requireMock("vscode");
+            const vscodeMock = jest.requireMock("vscode");
             const launchConfigManager = LaunchConfigManager.instance;
             vscodeMock.workspace.workspaceFolders = null;
             expect(launchConfigManager.getLaunchConfig()).toEqual('None');
@@ -71,18 +62,10 @@ describe("launchConfigManager", () => {
     });
 
     describe('configureLaunchJson', () => {
-        let fse: Mocked<typeof import("fs-extra")>;
-        let launchConfigManager: LaunchConfigManager;
-
-        beforeEach(async () => {
-            jest.doMock("fs-extra");
-            fse = require("fs-extra");
-            launchConfigManager = LaunchConfigManager.instance;
-            fse.readFileSync.mockImplementation((() => ''));
-        });
-
         it('adds a debug config to launch.json', async () => {
-            const vscodeMock = await jest.requireMock("vscode");
+            const vscodeMock = jest.requireMock("vscode");
+            const fse = jest.requireMock("fs-extra");
+            fse.readFileSync.mockImplementation((() => ''));
             vscodeMock.workspace.workspaceFolders = [{
                 uri:  'file:///g%3A/GIT/testPage'
             }];
@@ -96,15 +79,22 @@ describe("launchConfigManager", () => {
                 }
             });
             vscodeMock.Uri.joinPath = jest.fn();
+            const launchConfigManager = LaunchConfigManager.instance;
             launchConfigManager.configureLaunchJson();
             expect(vscodeMock.WorkspaceConfiguration.update).toBeCalledWith('configurations', expect.arrayContaining([expect.any(Object)]));
         });
 
         it('inserts a comment after the url property', async () => {
+            const fse = jest.requireMock("fs-extra");
             const expectedText = '\"url\":\"' + providedDebugConfig.url + '\" // Provide your project\'s url to finish configuring';
             fse.readFileSync.mockImplementation(() => JSON.stringify(providedDebugConfig));
+            const launchConfigManager = LaunchConfigManager.instance;
             await launchConfigManager.configureLaunchJson();
             expect(fse.writeFileSync).toHaveBeenCalledWith(expect.any(String), expect.stringContaining(expectedText));
         });
+    });
+
+    afterAll(() => {
+        jest.resetModules();
     });
 });
