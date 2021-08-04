a48e599f6ab2478ea5f84ebd6a9659bc43392b8f
Brandon Goddard <brgoddar@microsoft.com>
2021-08-04 07:56:10 -0700

    remove un-needed toolsWebSocket class

--- src/host_beta/host.ts
+++ src/host_beta/host.ts
@@ -1,16 +1,14 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { FrameToolsEvent, parseMessageFromChannel } from '../common/webviewEvents';
+import { encodeMessageForChannel, FrameToolsEvent, parseMessageFromChannel } from '../common/webviewEvents';
 import { ToolsHost } from './toolsHost';
-import { ToolsWebSocket } from './toolsWebSocket';
 
 declare const acquireVsCodeApi: () => {postMessage(message: unknown, args?: any|undefined): void};
 export const vscode = acquireVsCodeApi();
 
 export interface IDevToolsWindow extends Window {
     InspectorFrontendHost: ToolsHost;
-    WebSocket: typeof ToolsWebSocket;
 }
 
 export function initialize(dtWindow: IDevToolsWindow): void {
@@ -20,7 +18,8 @@ export function initialize(dtWindow: IDevToolsWindow): void {
 
     // Setup the global objects that must exist at load time
     dtWindow.InspectorFrontendHost = new ToolsHost();
-    dtWindow.WebSocket = ToolsWebSocket;
+    encodeMessageForChannel(msg => vscode.postMessage(msg, '*'), 'ready');
+
 
     let toolsWindow: Window | null;
 
@@ -31,16 +30,20 @@ export function initialize(dtWindow: IDevToolsWindow): void {
         }
     });
 
+
     const messageCallback =
         dtWindow.InspectorFrontendHost.onMessageFromChannel.bind(dtWindow.InspectorFrontendHost);
-    // Listen for messages from the extension and forward to the tools
+
+    // Both the DevTools iframe and the extension will post messages to the webview
+    // Listen for messages and forward to correct recipient based on origin
     dtWindow.addEventListener('message', messageEvent => {
         const fromExtension = messageEvent.origin.startsWith('vscode-webview://');
         console.log(messageEvent.origin)
         if (!fromExtension) {
-            // Call the right handler to send message to extension
+            // Send message from DevTools to Extension
             dtWindow.InspectorFrontendHost.onMessageFromFrame(messageEvent.data.method as FrameToolsEvent, messageEvent.data.args as any[]);
         } else if (toolsWindow) {
+            // Send message from Extension to DevTools
             parseMessageFromChannel(
                 messageEvent.data,
                 messageCallback,
