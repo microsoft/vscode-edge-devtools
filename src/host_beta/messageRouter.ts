e67829f5068162a4dff508b79813267bf722562a
Michael Liao <michael.liao@microsoft.com>
2021-08-24 07:48:42 -0700

    Checks ifCdn before enabling mirror editing + other fixes

--- src/host_beta/messageRouter.ts
+++ src/host_beta/messageRouter.ts
@@ -4,7 +4,7 @@
 import {
     encodeMessageForChannel,
     FrameToolsEvent,
-    ICssMirrroContentData,
+    ICssMirrorContentData,
     IOpenEditorData,
     parseMessageFromChannel,
     TelemetryData,
@@ -159,7 +159,7 @@ export class MessageRouter {
 
     private cssMirrorContent(url: string, newContent: string): void {
         // Forward the data to the extension
-        const request: ICssMirrroContentData = { url, newContent };
+        const request: ICssMirrorContentData = { url, newContent };
         encodeMessageForChannel(msg => vscode.postMessage(msg, '*'), 'cssMirrorContent', request);
     }
 
