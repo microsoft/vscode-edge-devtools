adfc4ae3f70465d3164d6780c36a2b3659a1b413
Vidal Ortega <43078681+vidorteg@users.noreply.github.com>
2021-08-25 09:45:42 -0700

    Introducing error reporter. (#474)
    This PR introduces an error reporting mechanism so that the user is aware of:
    
    feedback for the user from specific actions. e.g if a file is not mapped correctly, let the user know that it requires it's attention.
    https://i.imgur.com/wbkEp88.png
    
    errors or bugs that are not intended behavior and were the user can take action e.g a file that could not be opened because it does not exist anymore
    https://i.imgur.com/2DeqrnN.png
    
    For error/bugs the user is prompted to file a bug in github.
    
    Usage will be introduced in a following PR.
    Fixes #85

---
+++ src/errorReporter.ts
@@ -0,0 +1,68 @@
+
+import * as vscode from 'vscode';
+
+export const enum ErrorCodes {
+  Error,
+  Warning,
+  Informational
+}
+
+export interface ErrorEventInterface {
+  title: string,
+  message: string | unknown,
+  errorCode: ErrorCodes
+}
+
+export class ErrorReporter {
+  static async showErrorDialog(
+    error: ErrorEventInterface): Promise<void> {
+
+    const template = `<!-- Please delete any private information -->
+        **Version:**
+        ${// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+          vscode.extensions.getExtension('ms-edgedevtools.vscode-edge-devtools')?.packageJSON.version || 'N/A'}
+
+        **OS:**
+        <!-- Add your hosting platform e.g Microsoft Windows, WSL, MacOs or Linux -->
+
+        **Stack :**
+        ${error.message}
+
+        **Additional context:**
+        <!-- Add any other context or about the problem or screenshots here. -->`;
+
+    // cannot do multiline due to:
+    // https://github.com/Microsoft/vscode/issues/48900
+    const answer = await vscode.window
+      .showErrorMessage(
+        `An error occurred: ${error.title} ${error.message}`,
+        ...['File a bug']
+      );
+
+    if (answer === 'File a bug') {
+      let base = 'https://github.com/microsoft/vscode-edge-devtools/issues/new?';
+      const params: Map<string, string> = new Map<string, string>();
+
+      params.set('title',encodeURIComponent(`[${error.errorCode}] ${error.title}`));
+      params.set('body', encodeURIComponent(template));
+      params.set('labels', 'error');
+
+      // As this are GET request params there is no need to take out the last
+      // ampersand
+      params.forEach((value, key) => {
+        base += `${key}=${value}&`;
+      });
+
+      void vscode.env.openExternal(vscode.Uri.parse(base));
+    }
+  }
+
+  static async showInformationDialog(
+    error: ErrorEventInterface): Promise<void> {
+    // cannot do multiline due to:
+    // https://github.com/Microsoft/vscode/issues/48900
+    await vscode.window.showInformationMessage(
+      `${error.title} ${error.message}`
+    );
+  }
+}
