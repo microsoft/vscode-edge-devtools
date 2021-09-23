ee5ca00d7e4e1cd9bcb54524fabc48e3981746e7
Vidal Guillermo Diazleal Ortega <vidorteg@microsoft.com>
2021-09-23 11:56:06 -0700

    fixing licenses

--- src/screencast/input.ts
+++ src/screencast/input.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 import { ScreencastCDPConnection } from "./cdp";
 
 export const MouseEventMap = {
