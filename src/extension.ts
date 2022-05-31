a13cd6dca58e247ad2774fb888d4438aa8e020b2
Michael Liao <michael.liao@microsoft.com>
2022-05-31 14:57:05 -0700

    Add telemetry event for opening html via context menu

--- src/extension.ts
+++ src/extension.ts
@@ -228,6 +228,7 @@ export function activate(context: vscode.ExtensionContext): void {
     }));
 
     context.subscriptions.push(vscode.commands.registerCommand(`${SETTINGS_VIEW_NAME}.launchHtml`, (fileUri: vscode.Uri): void => {
+        telemetryReporter.sendTelemetryEvent('contextMenu/launchHtml');
         const edgeDebugConfig = providedHeadlessDebugConfig;
         const devToolsAttachConfig = providedLaunchDevToolsConfig;
         edgeDebugConfig.url = fileUri.fsPath;
