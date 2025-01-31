8bf86f9b9da4299d7499145c5b1e434b8bb6af15
Vidal Ortega <43078681+vidorteg@users.noreply.github.com>
2025-01-31 11:14:42 -0800

    Ramping up fallback version (#2740)
    Ramping up fallback version to latest Edge stable: 132.0.2957.140

--- src/utils.ts
+++ src/utils.ts
@@ -110,8 +110,8 @@ export const SETTINGS_DEFAULT_ENTRY_POINT = 'index.html';
 const WIN_APP_DATA = process.env.LOCALAPPDATA || '/';
 const msEdgeBrowserMapping: Map<BrowserFlavor, IBrowserPath> = new Map<BrowserFlavor, IBrowserPath>();
 
-// Current Revision: 127.0.2594.0
-export const CDN_FALLBACK_REVISION = '@f163ae219c3b08cda5aafa6b262442715a8a9893';
+// Current Revision: 132.0.2957.140
+export const CDN_FALLBACK_REVISION = '@a81e27b375d4f113f79997ac7f2bb49b93fbb84d';
 
 /** Build-specified flags. */
 declare const DEBUG: boolean;
